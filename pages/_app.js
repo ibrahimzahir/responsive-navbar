18c10ac11f582cc4dd9cf290e1b1f89e88cb0b4a
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2021-11-01 15:12:36 +0500

    header

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
-import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Responsive Navbar</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
-import Head from "next/head";
+import { Navbar } from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Responsive Navbar</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div>Responsive Menu</div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <p>Responsive Manu</p>
-      </footer>
+    <div>
+      <Navbar />
+      <div>Hello World</div>
     </div>
   );
 }
