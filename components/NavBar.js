beb68f700df9502ce6b0118a1859667f970e3fe2
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2021-11-06 14:41:25 +0500

    header completed

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,6 +2,12 @@ import Link from "next/link";
 import { useState } from "react";
 
 export const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <>
       <nav className="flex items-center flex-wrap bg-green-400 p-3 ">
@@ -19,7 +25,10 @@ export const Navbar = () => {
             </span>
           </a>
         </Link>
-        <button className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none">
+        <button
+          className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
+          onClick={handleClick}
+        >
           <svg
             className="w-6 h-6"
             fill="none"
@@ -35,7 +44,12 @@ export const Navbar = () => {
             />
           </svg>
         </button>
-        <div className="hidden w-full lg:inline-flex lg:flex-grow lg:w-auto">
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${
+            active ? "" : "hidden"
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
           <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
             <Link href="/">
               <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ">
